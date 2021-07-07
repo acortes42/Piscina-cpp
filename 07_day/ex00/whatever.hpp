8bb21f914f1f3b0cc8a4662407bab7646d61a87b
Adrian Cortes <diosdecarton@gmail.com>
2021-07-07 13:09:53 +0200

    Add 07 ex00

---
+++ 07_day/ex00/whatever.hpp
@@ -0,0 +1,16 @@
+# ifndef WHATEVER
+# define WHATEVER
+
+# include <iostream>
+# include <string>
+
+template <typename T> 
+T max(T n1, T n2);
+
+template <typename T> 
+T min(T n1, T n2);
+
+template <typename T> 
+void swap(T &n1, T& n2);
+
+#endif
